8a4d9302022f238e3c84dda6f129b4621f2e2d6f
yusuke <yusuke@KYOPCD08>
2019-03-14 17:34:30 +0900

    prov comment

--- calc.sh
+++ calc.sh
@@ -4,7 +4,8 @@
 sed -n '1p' data.csv > temp/header.csv
 # ヘッダー行を抜く
 # コンマ区切りで入出力
-# 3行目と4行目を計算して5行目に出力
+# 合計の計算
+# 明細金額を計算 3行目の数量と4行目の金額を掛ける
 # 合計行を追加
 cat data.csv | sed '1d' | awk -F',' 'BEGIN {FS=",";OFS=","}{sum+=$3*$4;print $1,$2,$3,$4,$3*$4} END{print "","","","合計",sum}' > temp/temp.csv
 # ヘッダーとデータを結合
